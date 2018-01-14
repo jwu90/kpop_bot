9e48189b8e159a20ad30da401e07e5accc7696d0
Justin Wu <jwu90@ucsc.edu>
2018-01-13 17:19:15 -0800

    first commit

---
+++ credentials.py
@@ -0,0 +1,4 @@
+consumer_key = 'insertConsumerkey'
+consumer_secret = 'insertConsumer_secret'
+access_token = 	'insertAccess_token'
+access_token_secret = 'insertAccess_token_Secret'
---
+++ koreanbot.py.py
@@ -0,0 +1,32 @@
+import tweepy
+from time import sleep
+from credentials import *
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+for tweet in tweepy.Cursor(api.search, q='#kpop').items():
+    try:
+        # Add \n escape character to print() to organize tweets
+        print('\nTweet by: @' + tweet.user.screen_name)
+
+        # Retweet tweets as they are found
+        tweet.retweet()
+        print('Retweeted the tweet')
+
+        # Favorite the tweet
+        tweet.favorite()
+        print('Favorited the tweet')
+
+        # Follow the user who tweeted
+        tweet.user.follow()
+        print('Followed the user')
+
+        sleep(15)
+
+    except tweepy.TweepError as e:
+        print(e.reason)
+
+    except StopIteration:
+        break
